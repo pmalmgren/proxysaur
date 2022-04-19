21e1330b26b4b7feda7ba3ebe8547ad234236f4b
Peter Malmgren <ptmalmgren@gmail.com>
2022-04-19 13:31:14 -0400

    Add a custom tunnel function

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,17 @@
 use anyhow::Result;
 use clap::StructOpt;
-use config::{Args, Config, Proxy};
-use futures::future::{join_all, try_join_all};
-use tokio::net::{TcpListener, TcpStream};
+use config::{Args, Config};
 
 mod config;
-
-async fn tunnel(socket: &mut TcpStream, upstream: &mut TcpStream) -> Result<()> {
-    tokio::io::copy_bidirectional(upstream, socket)
-        .await
-        .map(|_| ())
-        .map_err(anyhow::Error::from)
-}
-
-async fn proxy_conn(mut socket: TcpStream, proxy: Proxy) -> Result<()> {
-    let mut upstream = TcpStream::connect(&proxy.upstream_address()).await?;
-    tunnel(&mut socket, &mut upstream).await
-}
-
-async fn listen(listener: TcpListener, proxy: Proxy) {
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        let proxy = proxy.clone();
-        tokio::spawn(async move {
-            if let Err(err) = proxy_conn(socket, proxy).await {
-                eprintln!("Error proxying the connection: {err}");
-            }
-        });
-    }
-}
-
-async fn bind(proxy: Proxy) -> Result<(TcpListener, Proxy)> {
-    TcpListener::bind(&proxy.address())
-        .await
-        .map(|listener| (listener, proxy))
-        .map_err(anyhow::Error::from)
-}
+mod proxy;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let subscriber = tracing_subscriber::fmt::Subscriber::new();
+    tracing::subscriber::set_global_default(subscriber).expect("should set subscriber");
+
     let args = Args::parse();
     let config = Config::try_from(args)?;
 
-    let futures = config
-        .proxy
-        .into_iter()
-        .map(|proxy| async move { bind(proxy).await });
-
-    let listeners = try_join_all(futures).await?;
-
-    let _handle = join_all(
-        listeners
-            .into_iter()
-            .map(|(listener, proxy)| async move { listen(listener, proxy).await }),
-    )
-    .await;
-
-    Ok(())
+    proxy::run(config).await
 }
