1bceb22c5fa54ffee11cc39d30595f147bf02a7a
Peter Malmgren <ptmalmgren@gmail.com>
2022-04-19 09:33:23 -0400

    Pass proxy configuration to listener

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use clap::StructOpt;
-use config::{Args, Config};
+use config::{Args, Config, Proxy};
 use futures::future::{join_all, try_join_all};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -17,7 +17,7 @@ async fn respond(socket: &mut TcpStream) {
     };
 }
 
-async fn process(mut socket: TcpStream) {
+async fn proxy_conn(mut socket: TcpStream) {
     let mut buf = vec![0; 1024];
     match socket.read(&mut buf).await {
         Ok(nread) => {
@@ -28,15 +28,20 @@ async fn process(mut socket: TcpStream) {
     }
 }
 
-async fn listen(listener: TcpListener) {
+async fn listen(listener: TcpListener, _proxy: Proxy) {
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        // A new task is spawned for each inbound socket. The socket is
-        // moved to the new task and processed there.
         tokio::spawn(async move {
-            process(socket).await;
+            proxy_conn(socket).await;
         });
     }
+}
+
+async fn bind(proxy: Proxy) -> Result<(TcpListener, Proxy)> {
+    TcpListener::bind(&proxy.address())
+        .await
+        .map(|listener| (listener, proxy))
+        .map_err(anyhow::Error::from)
 }
 
 #[tokio::main]
@@ -47,15 +52,16 @@ async fn main() -> Result<()> {
     let futures = config
         .proxy
         .into_iter()
-        .map(|proxy| async move { TcpListener::bind(proxy.address()).await });
+        .map(|proxy| async move { bind(proxy).await });
 
     let listeners = try_join_all(futures).await?;
 
-    let _handle =
-        join_all(listeners.into_iter().map(|listener| async move {
-            tokio::spawn(async move { listen(listener).await }).await
-        }))
-        .await;
+    let _handle = join_all(
+        listeners
+            .into_iter()
+            .map(|(listener, proxy)| async move { listen(listener, proxy).await }),
+    )
+    .await;
 
     Ok(())
 }
