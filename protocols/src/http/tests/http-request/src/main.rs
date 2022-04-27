ca93a030ba4c2236ee5dc62579b3063c9c76d419
Peter Malmgren <ptmalmgren@gmail.com>
2022-04-27 11:46:26 -0400

    Add better tests

--- protocols/src/http/tests/http-request/src/main.rs
+++ protocols/src/http/tests/http-request/src/main.rs
@@ -5,7 +5,8 @@ wit_bindgen_rust::import!("../../request.wit");
 fn main() {
     let request: HttpRequest = request::http_request_get().expect("should get the request");
 
-    if request.method.to_lowercase() == "get" {
+    if request.host == "petermalmgren.com" {
+    } else if request.method.to_lowercase() == "get" {
         request::http_request_set_method("post").expect("should set the method");
         request::http_request_set_body("haha!".as_bytes()).expect("should set the body");
     }
