b1103a1bd1eff612dc05739598607ff2a1591719
Peter Malmgren <ptmalmgren@gmail.com>
2022-05-01 11:08:09 -0400

    Add better port parsing

--- protocols/src/http/hostname.rs
+++ protocols/src/http/hostname.rs
@@ -19,16 +19,24 @@ impl TryFrom<&Request<Body>> for Hostname {
             .ok_or_else(|| anyhow::Error::msg("Missing authority"))?;
         let authority = host.as_str().to_string();
         let parts: Vec<&str> = host.as_str().split(':').collect();
-        if parts.len() != 2 {
-            tracing::error!(%authority, "Hostname missing port.");
-            return Err(anyhow::Error::msg("Missing port"));
-        }
-        let port: u16 = match parts[1].parse() {
-            Ok(v) => v,
-            Err(_error) => {
-                tracing::error!(%authority, "Hostname missing port.");
-                return Err(anyhow::Error::msg("Missing port"));
+        let port: Option<u16> = if parts.len() > 1 {
+            match parts[1].parse::<u16>() {
+                Ok(port) => Some(port),
+                Err(_) => None,
             }
+        } else {
+            None
+        };
+        let port: u16 = match port {
+            Some(v) => v,
+            None => match req.uri().scheme_str() {
+                Some("http") => 80,
+                Some("https") => 443,
+                Some(_) | None => {
+                    tracing::error!(%authority, "Hostname missing port.");
+                    return Err(anyhow::Error::msg("Missing port"));
+                }
+            },
         };
         let host = parts[0].to_string();
         Ok(Hostname {
